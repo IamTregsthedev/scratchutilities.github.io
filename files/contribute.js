8159d5237636ad69672f0516221bcd64f7a675e4
Arav Narula <proteximportsexports@gmail.com>
2020-02-13 16:49:34 -0500

    Create contribute.js

---
+++ files/contribute.js
@@ -0,0 +1,98 @@
+var contribUsersList = {
+  "content-order": [
+    "KennethSweezy",
+    "Andreto",
+    "space-elephant",
+    "IamTregsthedev",
+    "jamesbmadden",
+    "Chexbox",
+    "ncomendant",
+    "appy9",
+    "RainbowLegend",
+    "Avantika-Didroy",
+    "jokari",
+    "locness3",
+    "MS72"
+  ],
+
+  content: {
+    KennethSweezy: {
+      username: "Thermodynamical",
+      userid: "33377824",
+      link: "https://scratch.mit.edu/users/Thermodynamical/",
+      team: "1"
+    },
+    Andreto: {
+      username: "Andreto",
+      userid: "28541538",
+      link: "https://andreto.github.io/",
+      team: "1"
+    },
+    Chexbox: {
+      username: "Chexbox",
+      userid: "31783961",
+      link: "https://github.com/Chexbox",
+      team: "0"
+    },
+    "space-elephant": {
+      username: "space-elephant",
+      userid: "45874738",
+      link: "https://github.com/space-elephant",
+      team: "1"
+    },
+    ncomendant: {
+      username: "ncomendant",
+      userid: "3222456",
+      link: "https://github.com/ncomendant",
+      team: "0"
+    },
+    appy9: {
+      username: "appy9",
+      userid: "15341869",
+      link: "https://github.com/appy9",
+      team: "0"
+    },
+    RainbowLegend: {
+      username: "RainbowLegend",
+      userid: "26859432",
+      link: "https://github.com/RainbowLegend",
+      team: "0"
+    },
+    "Avantika-Didroy": {
+      username: "Avantika-Didroy",
+      userid: "44540293",
+      link: "https://github.com/Avantika-Didroy",
+      team: "0"
+    },
+    jokari: {
+      username: "jokari",
+      userid: "11021734",
+      link: "https://github.com/jokari",
+      team: "0"
+    },
+    IamTregsthedev: {
+      username: "Arav Narula",
+      userid: "21000033",
+      link: "https://aravnarula.com",
+      team: "1"
+    },
+    MS72: {
+      username: "M-S-72",
+      userid: "46165762",
+      link: "https://github.com/M-S-72",
+      team: "0"
+    },
+    locness3: {
+      username: "Locness",
+      userid: "37651007",
+      link: "https://locness3.github.io",
+      team: "0"
+    },
+    jamesbmadden: {
+      username: "jamesbmadden",
+      userid: "39145924",
+      link: "https://github.com/jamesbmadden",
+      team: "0"
+    }
+  }
+};
